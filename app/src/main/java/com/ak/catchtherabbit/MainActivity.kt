8e4cc935de229d4550e731267b75af2b85426dd6
hasan <hasan_ak_44@hotmail.com>
2021-12-31 21:37:46 +0300

    alert message fix

--- app/src/main/java/com/ak/catchtherabbit/MainActivity.kt
+++ app/src/main/java/com/ak/catchtherabbit/MainActivity.kt
@@ -71,7 +71,7 @@ class MainActivity : AppCompatActivity() {
                   startActivity(intent)
 
               }
-              alert.setNegativeButton("Nos"){dialog,wich ->
+              alert.setNegativeButton("No"){dialog,wich ->
                   finish()
 
               }
